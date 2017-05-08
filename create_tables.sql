9c9cf6b3b1446380a2f3febafa6f547ad4ec77ef
Albert Morgan <morgaalb@oregonstate.edu>
2017-05-08 14:48:57 -0700

    Initial commit of create_tables.sql

---
+++ create_tables.sql
@@ -0,0 +1,76 @@
+DROP TABLE IF EXISTS addresses;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS billing_info;
+DROP TABLE IF EXISTS subscriptions;
+DROP TABLE IF EXISTS plans;
+DROP TABLE IF EXISTS address_plans;
+DROP TABLE IF EXISTS employees;
+
+CREATE TABLE customers (
+	id				INT AUTO_INCREMENT NOT NULL,
+	username		VARCHAR(255) UNIQUE NOT NULL,
+	name			VARCHAR(255) NOT NULL,
+	address_id		INT,
+	PRIMARY KEY (id),
+	FOREIGN KEY (address_id) REFERENCES addresses (id)
+);
+
+CREATE TABLE billing_info (
+	id				INT AUTO_INCREMENT NOT NULL,
+	cc_number		DECIMAL(16,0) NOT NULL,
+	cc_type			VARCHAR(10) NOT NULL,
+	expiration_date	DATE NOT NULL,
+	name			VARCHAR(255) NOT NULL,
+	user_id			INT NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (user_id) REFERENCES customers(id)
+);
+
+CREATE TABLE subscriptions (
+	billing_id		INT,
+	customer_id		INT,
+	plan_id			INT,
+	address_Id		INT,
+	PRIMARY KEY (plan_id, address_id), -- Can't subscribe to the same plan at the same address
+	FOREIGN KEY (billing_id) REFERENCES billing_info(id),
+	FOREIGN KEY (customer_id) REFERENCES customers(id),
+	FOREIGN KEY (address_id) REFERENCES addresses(id)
+);
+
+CREATE TABLE plans (
+	id				INT AUTO_INCREMENT NOT NULL,
+	name			VARCHAR(255),
+	price			DECIMAL(5, 2),
+	speed			INT,
+	added_by		INT,
+	PRIMARY KEY (id),
+	FOREIGN KEY (added_by) REFERENCES employees(id)
+);
+
+CREATE TABLE addresses (
+	id				INT AUTO_INCREMENT NOT NULL,
+	num				INT NOT NULL,
+	street			VARCHAR(255) NOT NULL,
+	st				CHAR(2) NOT NULL,
+	city			VARCHAR(255) NOT NULL,
+	zip				DECIMAL(5) NOT NULL,
+	apt_no			DECIMAL(3),
+	PRIMARY KEY (id)
+);
+
+-- Junction table between addresses and plans
+-- These are which plans are available at which addresses
+-- (as opposed to subscriptions, which is actual subscriptions)
+CREATE TABLE address_plans (
+	plan_id			INT NOT NULL,
+	address_id		INT NOT NULL,
+	PRIMARY KEY (plan_id, address_id)
+);
+
+CREATE TABLE employees (
+	id				INT AUTO_INCREMENT NOT NULL,
+	username		VARCHAR(255) UNIQUE NOT NULL,
+	salary			INT,
+	name			VARCHAR(255),
+	PRIMARY KEY	(id)
+);
