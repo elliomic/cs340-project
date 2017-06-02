5523967c342fe12f5fdd196c68fbe34c24950d69
morgaalb <morgaalb@oregonstate.edu>
2017-06-02 10:19:42 -0700

    Added employee login logic

---
+++ public/doemplogin.php
@@ -0,0 +1,32 @@
+<?php
+// change the value of $dbuser and $dbpass to your username and password
+	include 'connectvarsEECS.php'; 
+	
+	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	if (!$conn) {
+		die('Could not connect: ' . mysqli_error());
+	}
+
+	$user = mysqli_real_escape_string($conn, $_POST['user']);
+	$password = mysqli_real_escape_string($conn, md5($_POST["password"]));
+
+	$result = mysqli_query($conn, "SELECT * FROM Employee WHERE username = '" . $user . "' AND pass = '" . $password . "'");
+	if (!$result) {
+		die("Query to show fields from table failed");
+	}
+	$num_row = mysqli_num_rows($result);
+
+	if($num_row > 0) {
+		session_start();
+		$_SESSION['type'] = 'cutsomer';
+		$_SESSION['user'] = $user;
+		header('Location: ./plans.php');
+	} else {
+		header('Location: ./login.php?failed');
+	}
+	
+
+	mysqli_free_result($result);
+	mysqli_close($conn);
+	?>
+
--- public/employee_login.php
+++ public/employee_login.php
@@ -1,13 +1,46 @@
 <?php include '_header.php' ?>
 
-<h1> Log In </h1>
+<!--
+<h1> Sign Up </h1>
+
 <form action="insert.php"  method="post">
   Username:
-  <input type="text" name="username" placeholder="smithers" autocomplete=off pattern=".{5,20}" required title="5 to 20 characters" ><br>
+  <input type="text" name="username" placeholder="bentown" autocomplete=off pattern=".{5,20}" required title="5 to 20 characters" ><br>
+  First name:
+  <input type="text" name="firstname" placeholder="benny" autocomplete=off pattern=".{5,20}" required title="5 to 20 characters"><br>
+  Last name:
+  <input type="text" name="lastname"placeholder="beaver" autocomplete=off pattern=".{5,20}" required title="5 to 20 characters"><br>
   Password:
   <input type="password" name="password"placeholder="*******" autocomplete=off pattern=".{5,20}" required title="5 to 20 characters"><br>
+  Email:
+  <input type="text" name="email" placeholder="email@gmail.com" autocomplete=off pattern=".{5,20}" required title="5 to 20 characters"><br>
+  Age:
+  <input type="text" name="age" placeholder="25" autocomplete=off ><br>
 
 <input type="submit" name="submit">
 </form>
+-->
+
+<h1> Log In </h1>
+
+<?php
+
+	if(isset($_GET['failed'])) {
+		echo '<div class="loginfailed">Login failed</div>';
+	}
+
+?>
+
+<form action="doemplogin.php"  method="post">
+  Username:
+  <input type="text" name="user" autocomplete=off pattern=".{3,20}" required title="3 to 20 characters" ><br>
+  Password:
+  <input type="password" name="password" autocomplete=off pattern=".{5,20}" required title="5 to 20 characters"><br>
+
+<input type="submit" name="submit">
+</form>
+
+
+
 
 <?php include '_footer.php' ?>
