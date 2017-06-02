6bd64f9403648eb657469427a5f2942b277463fc
Michael Elliott <elliomic@onid.orst.edu>
2017-06-02 14:00:29 -0700

    Added help page

---
+++ public/help.php
@@ -0,0 +1,15 @@
+<?php include("_header.php"); ?>
+<br>
+<h2>Welcome to Pebcak!</h2>
+<p>Here we've answered some common questions our customers have about how to use our site:</p>
+
+<h4>Q: How do I sign up?</h4>
+<p>A: Visit our <a href="add.php">account creation page</a> to create an account. Then visit our <a href="plans.php">plan list</a> to pick a plan.</p>
+
+<h4>Q: How do I log in to my account?</h4>
+<p>A: Visit our <a href="login.php">account login page</a> to log in to your account.</p>
+
+<h4>Q: How do I view available plans?</h4>
+<p>A: Visit our <a href="plans.php">plan list page</a> to view available plans.</p>
+
+<?php include("_footer.php"); ?>
