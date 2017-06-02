d8bb7e7b94f46a845e490d4e8b98345a360bf4e6
morgaalb <morgaalb@oregonstate.edu>
2017-06-02 11:06:15 -0700

    Added logic that shows which plans are available at a customer's address

--- public/plans.php
+++ public/plans.php
@@ -1,31 +1,64 @@
+<?php session_start() ?>
 <?php include '_header.php' ?>
 <?php
-// change the value of $dbuser and $dbpass to your username and password
+
+
+//r change the value of $dbuser and $dbpass to your username and password
+	error_reporting(E_ALL); ini_set('display_errors', 1);
+
 	include 'connectvarsEECS.php'; 
 	
 	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 	if (!$conn) {
 		die('Could not connect: ' . mysqli_error());
 	}
-	$result = mysqli_query($conn, "SELECT * FROM Plan");
+
+	$user = "";
+	$loggedIn = False;
+
+	if(isset($_SESSION['type']) && isset($_SESSION['user']) && $_SESSION['type'] == 'customer') {
+		$user = mysqli_real_escape_string($conn, $_SESSION['user']);
+		$loggedIn = True;
+	}
+
+	// $result = mysqli_query($conn, "SELECT p.name, p.price, p.speed, c.id FROM Plan p LEFT JOIN Address_Plans ap ON p.id = ap.plan_id LEFT JOIN Customer c ON ap.address_id = c.address_id AND c.username = '" . $user . "'");
+
+
+	$result = mysqli_query($conn, "SELECT p.name, p.price, p.speed, c.id FROM Plan p LEFT JOIN Customer c ON c.address_id IN (SELECT address_id FROM Address_Plans WHERE address_id = c.address_id AND plan_id = p.id AND c.id = 1)");
+
+
+	$num_row = mysqli_num_rows($result);
+	
 	if (!$result) {
 		die("Query to show fields from table failed");
 	}
-	$num_row = mysqli_num_rows($result);	
 	
 	echo "<table>";
 	echo "<tr><th>Plan Name</th><th>Speed</th><th>Price / Mo</th>";
 
-	if(isset($_SESSION['type']) && isset($_SESSION['user']) && $_SESSION['type'] == 'customer') {
+	if($loggedIn) {
+		echo "<th>Available at your location</th>";
 	}
 
 	echo "</tr>";
-	// echo "<form action=\"showTable.php\" method=\"POST\">";
-	// echo "<select name=\"table\" size=\"1\" Font size=\"+2\">";
 	// Select a database table to display
 	for($i=0; $i<$num_row; $i++) {
 		$plan=mysqli_fetch_row($result);
-		echo "<tr><td>" . $plan[1] . "</td><td>" . $plan[3] . "</td><td>" . $plan[2] . "</td><tr>";
+		echo "<tr><td>" . $plan[0] . "</td><td>" . $plan[2] . "</td><td>" . $plan[1] . "</td>";
+		
+		if($loggedIn) {
+			echo "<td>";
+
+			if(isset($plan[3])) {
+				echo '<div class="planYes">Yes!</div>';
+			} else {
+				echo '<div class="planNo">No</div>';
+			}
+
+			echo "</td>";
+		}
+		
+		echo "<tr>";
 		// echo "<option value='$tablename[0]' >".$tablename[0]."</option>";
 	}
 	
