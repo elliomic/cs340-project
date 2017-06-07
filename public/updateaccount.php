5454c8063830b8f945987bf68ebfb7b7a68ac14f
morgaalb <morgaalb@oregonstate.edu>
2017-06-07 12:54:12 -0700

    Started code for updating customer address

--- public/updateaccount.php
+++ public/updateaccount.php
@@ -12,14 +12,28 @@
 	$user = mysqli_real_escape_string($conn, $_POST['user']);
 	$id = mysqli_real_escape_string($conn, $_SESSION['id']);
 	
+	$num = mysqli_real_escape_string($conn, $_SESSION['num']);
+	$street = mysqli_real_escape_string($conn, $_SESSION['street']);
+	$apt = mysqli_real_escape_string($conn, $_SESSION['apt']);
+	$city = mysqli_real_escape_string($conn, $_SESSION['city']);
+	$state = mysqli_real_escape_string($conn, $_SESSION['state']);
+	$zip = mysqli_real_escape_string($conn, $_SESSION['zip']);
+	
 	$name = mysqli_real_escape_string($conn, $_POST['name']);
 	
 	$result = mysqli_query($conn, "UPDATE Customer SET name = '" . $name . "' WHERE id = " . $_SESSION['id']);
 
 	if (!$result) {
-		die("Query to show fields from table failed");
+		die("Query to update name failed.");
 	}
 	// $num_row = mysqli_num_rows($result);
+	
+	// Get the address id of the customer
+	$result = mysqli_query($conn, "SELECT address_id FROM Customer WHERE id = " . $_SESSION['id']);
+	$userInfo = mysqli_fetch_row($result);
+	$addressId = $userInfo[0];
+	
+	$result = mysqli_query($conn, "UPDATE A"
 
 	mysqli_free_result($result);
 	mysqli_close($conn);
