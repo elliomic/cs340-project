b67a6f469a7bdbf118233f43dcfce59a7948ba54
hayden anderson <hayden anderson>
2017-06-08 16:55:04 -0700

    merged publics

--- public/_header.php
+++ public/_header.php
@@ -1,27 +1,17 @@
-<?php session_start() ?>
-<html>
-<head>
-<title>Pebcak</title>
-<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-<div id="wrap">
-<h1> Pebcak Enterprises </h1>
-<nav>
-<ul id="menu">
-<li><a href="index.php">Home</a> </li>
-<li><a href="plans.php">Browse Plans</a></li>
-
-<?php
-	if (isset($_SESSION['type']) && $_SESSION['type'] == 'customer') {
-		echo '<li><a href="account.php">My Account</a></li>';
-		echo '<li><a href="logout.php">Log Out</a></li>';
-	} else {
-		echo '<li><a href="login.php">Login</a></li>';
-	}
-?>
-
-<li><a href="employee_login.php">Employees</a></li>
-</ul> 
-</nav>
-<main>
+<html lang="en">
+  <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>PEBCAK</title>
+	<link rel="stylesheet" href="includes/css/all.css">
+  </head>
+  <body>
+    <?php include("_nav.php"); ?>
+	<main role="main">
+	  
+	  <section class="x-content-banner x-content-banner--pad-top-sm x-content-banner--pad-bottom-sm x-ui-theme--transparent x-background--transparent">
+		
+		<div class="x-flex-row middle-md center-sm">
+		  
+		  <div class="x-flex__col-xs-12 x-flex__col-md-8 x--ta-c">
+			<div class="x-flex__content x-content-banner__content">
--- public/connectvarsEECS.php
+++ public/connectvarsEECS.php
@@ -1,7 +1,7 @@
 <?php
   // Define database connection constants
   define('DB_HOST', 'classmysql.engr.oregonstate.edu');
-  define('DB_USER', 'cs340_morgaalb');
-  define('DB_PASSWORD', '8365');
-  define('DB_NAME', 'cs340_morgaalb');
+  define('DB_USER', 'cs340_anderrob');
+  define('DB_PASSWORD', '.....');
+  define('DB_NAME', 'cs340_anderrob');
 ?>
