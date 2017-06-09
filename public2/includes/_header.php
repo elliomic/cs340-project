4b9217260ec1a30e223b253cb5488773c0cd0e59
Michael Elliott <elliomic@onid.orst.edu>
2017-06-08 17:22:41 -0700

    Moved css and images folders into parent. Updated header

--- public2/includes/_header.php
+++ public2/includes/_header.php
@@ -1,22 +1,25 @@
 <?php require_once "includes/setup.php" ?>
-
+<!DOCTYPE HTML>
 <html lang="en">
   <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>PEBCAK</title>
-	<link rel="stylesheet" href="includes/css/all.css">
+	<link rel="stylesheet" href="css/all.css">
+    <link rel="icon"
+          type="image/png"
+          href="images/favicon.png">
   </head>
   <body>
     
     <header class="x-header-nav x-header-nav--ad">
-	  <link href="includes/css/gnavstyle.css" rel="stylesheet">
+	  <link href="css/gnavstyle.css" rel="stylesheet">
 	  <div aria-label="Main Navigation" class="polaris-nav polaris-nav-header-wrapper" role="navigation">		
 		<div class="polaris-header"> 
 		  <ul class="polaris-navigation">
 			<li class="polaris-item">
 			  <a href="index.php" target="_self" class="polaris-link ">
-				<img src="includes/images/logo.jpg" height=50>
+				<img src="images/logo.jpg" height=50>
 			  </a>
 			</li>
 		  </ul>
@@ -31,17 +34,25 @@
 				<span class="polaris-trackname">Shop</span>
 			  </a>
 			</li>
+            <?php if(is_logged_in()) {?>
 			<li class="polaris-item ">
 			  <a href="account.php " target="_self" class="polaris-link ">
 				<span class="polaris-trackname">Account</span>
 			  </a>
 			</li>
+            <?php } ?>
 		  </ul>
 		  <ul class="polaris-personal">
 			<li class="polaris-item ">
+              <?php if(is_logged_in()) {?>
+			  <a href="logout.php" target="_self" class="polaris-link ">
+				<span class="polaris-trackname">Log Out</span>
+			  </a>
+              <?php } else {?>
 			  <a href="signin.php" target="_self" class="polaris-link ">
 				<span class="polaris-trackname">Sign In</span>
 			  </a>
+              <?php } ?>
 			</li>
 		  </ul>
 		</div>
