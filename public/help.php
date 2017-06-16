1076cc2e138cf1921604f1cb603da535b8a352b3
Michael Elliott <elliomic@onid.orst.edu>
2017-06-15 20:09:36 -0700

    Update help page and added favicon

--- public/help.php
+++ public/help.php
@@ -1,15 +1,21 @@
 <?php include("_header.php"); ?>
+
+<h2 class="x-display2">Welcome to Pebcak!</h2>
+<p class="x-heading2">Here we've answered some common questions our customers have about how to use our site:</p>
 <br>
-<h2>Welcome to Pebcak!</h2>
-<p>Here we've answered some common questions our customers have about how to use our site:</p>
-<br>
-<h4>Q: How do I sign up?</h4>
-<p>A: Visit our <a href="add.php">account creation page</a> to create an account. Then visit our <a href="plans.php">plan list</a> to pick a plan.</p>
-<br>
-<h4>Q: How do I log in to my account?</h4>
-<p>A: Visit our <a href="login.php">account login page</a> to log in to your account.</p>
-<br>
-<h4>Q: How do I view available plans?</h4>
-<p>A: Visit our <a href="plans.php">plan list page</a> to view available plans.</p>
+<div class="cui-article-body">
+  <h2 class="cui-article-section-title">Q: How do I sign up?</h2>
+  <p class="cui-article-section-message">Call us at 1-800-7-PEBCAK (1-800-773-2225) to create an account. Then visit our <a href="plans.php">plan list</a> to pick a plan.</p>
+  <br>
+  <h2 class="cui-article-section-title">Q: How do I log in to my account?</h2>
+  <p class="cui-article-section-message">Visit our <a href="login.php">account login page</a> to log in to your account.</p>
+  <br>
+  <h2 class="cui-article-section-title">Q: How do I view available plans?</h2>
+  <p class="cui-article-section-message">Visit our <a href="plans.php">plan list page</a> to view available plans.</p>
+  <br>
+  <h2 class="cui-article-section-title">Q: How do I update my account information?</h2>
+  <p class="cui-article-section-message">Visit your <a href="account.php">account page</a> to view and edit your account information.</p>
+
+</div>
 
 <?php include("_footer.php"); ?>
