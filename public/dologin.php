3d568b36d08286101059facc67157836e52eb95d
hayden anderson <hayden anderson>
2017-06-15 19:00:28 -0700

    added employee account stuff

--- public/dologin.php
+++ public/dologin.php
@@ -6,27 +6,47 @@
 	if (!$conn) {
 		die('Could not connect: ' . mysqli_error());
 	}
-
+	$emp = 	mysqli_real_escape_string($conn, $_POST['employee']);
 	$user = mysqli_real_escape_string($conn, $_POST['user']);
 	$password = mysqli_real_escape_string($conn, md5($_POST["password"]));
 
-	$result = mysqli_query($conn, "SELECT * FROM Customer WHERE username = '" . $user . "' AND pass = '" . $password . "'");
-	if (!$result) {
-		die("Query to show fields from table failed");
-	}
-	$num_row = mysqli_num_rows($result);
+	if ($emp == TRUE){
+			$result = mysqli_query($conn, "SELECT * FROM Employee WHERE username = '" . $user . "' AND pass = '" . $password . "'");
+		if (!$result) {
+			die("Query to show fields from table failed");
+		}
+		$num_row = mysqli_num_rows($result);
 
-	if($num_row > 0) {
-		session_start();
-		$_SESSION['type'] = 'customer';
-		$_SESSION['user'] = $user;
-		$row = mysqli_fetch_row($result);
-		$_SESSION['id'] = $row[0];
-		header('Location: ./account.php');
-	} else {
-		header('Location: ./login.php?failed');
+		if($num_row > 0) {
+			session_start();
+			$_SESSION['type'] = 'employee';
+			$_SESSION['user'] = $user;
+			$row = mysqli_fetch_row($result);
+			$_SESSION['id'] = $row[0];
+			header('Location: ./employee_account.php');
+		} else {
+			header('Location: ./login.php?failed');
+		}
 	}
-	
+	else{
+		
+		$result = mysqli_query($conn, "SELECT * FROM Customer WHERE username = '" . $user . "' AND pass = '" . $password . "'");
+		if (!$result) {
+			die("Query to show fields from table failed");
+		}
+		$num_row = mysqli_num_rows($result);
+
+		if($num_row > 0) {
+			session_start();
+			$_SESSION['type'] = 'customer';
+			$_SESSION['user'] = $user;
+			$row = mysqli_fetch_row($result);
+			$_SESSION['id'] = $row[0];
+			header('Location: ./account.php');
+		} else {
+			header('Location: ./login.php?failed');
+		}
+	}
 
 	mysqli_free_result($result);
 	mysqli_close($conn);
