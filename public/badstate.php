6c98b7b05f5fc181295cab907b448fbd1761a723
Alexander Ruef <ruefa@flip1.engr.oregonstate.edu>
2017-06-15 21:54:44 -0700

    added some comments

--- public/badstate.php
+++ public/badstate.php
@@ -1,5 +1,6 @@
 <?php include '_header.php' ?>
 <?php
+//directed here if you entered a bad state for your address
 echo '<div style="color:red">Error: that is not a valid state</div>';
 echo '<br>';
 echo '<a href="account.php">Back to account</a>';
--- public/doemplogin.php
+++ public/doemplogin.php
@@ -10,12 +10,14 @@
 	$user = mysqli_real_escape_string($conn, $_POST['user']);
 	$password = mysqli_real_escape_string($conn, md5($_POST["password"]));
 
+	//checks if username and password match
 	$result = mysqli_query($conn, "SELECT * FROM Employee WHERE username = '" . $user . "' AND pass = '" . $password . "'");
 	if (!$result) {
 		die("Query to show fields from table failed");
 	}
 	$num_row = mysqli_num_rows($result);
 
+	//start the session cookie
 	if($num_row > 0) {
 		session_start();
 		$_SESSION['type'] = 'employee';
