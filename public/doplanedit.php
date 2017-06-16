5fedd3bbf4a1ea5b0a651af02b3bbdab687cc263
Alexander Ruef <ruefa@flip1.engr.oregonstate.edu>
2017-06-15 19:35:48 -0700

    removed debugging prints

--- public/doplanedit.php
+++ public/doplanedit.php
@@ -12,12 +12,6 @@
 	$added_by = mysqli_real_escape_string($conn, $_POST['added_by']);
 	$planID = mysqli_real_escape_string($conn, $_POST['planID']);
 
-	echo $name.'<br>';
-	echo $price.'<br>';
-	echo $speed.'<br>';
-	echo $added_by.'<br>';
-	echo $planID.'<br>';
-
 	$result = mysqli_query($conn, "UPDATE Plan SET name = '" . $name . "', price = " . $price . ", speed = " . $speed . ", added_by = " . $added_by . " WHERE id = " . $planID);
 	
 	if($result){
